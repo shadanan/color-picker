a858c74a0a622c6dad53f4057ac3af246397f642
Shad Sharma <shadanan@gmail.com>
2024-04-29 22:14:45 -0700

    Copy to clipboard button

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
+  import { IroColor } from "@irojs/iro-core";
   import iro from "@jaames/iro";
   import { onMount } from "svelte";
 
-  let color: string = "#000000";
+  let color: iro.Color = new IroColor("#000000");
+  $: style = `background-color: ${color.hexString}; 
+    color: ${(color.hsv.v || 0) < 75 ? "white" : "black"}`;
+  let copying = false;
 
   let colorPicker: iro.ColorPicker;
   onMount(() => {
     colorPicker = iro.ColorPicker("#picker", {
-      color,
+      color: color.hexString,
       width: 400,
       borderWidth: 1,
       layout: [
@@ -24,31 +28,36 @@
     });
 
     colorPicker.on("color:change", (nextColor: iro.Color) => {
-      color = nextColor.hexString;
+      color = nextColor;
     });
   });
 
   function copy() {
-    navigator.clipboard.writeText(color);
+    navigator.clipboard.writeText(color?.hexString);
+    copying = true;
+    setTimeout(() => (copying = false), 500);
   }
 </script>
 
 <main>
   <div id="picker"></div>
   <div id="select">
-    <label for="color">Selected Color</label>
-    <div id="color" style="background-color: {color};">
-      <h1>{color}</h1>
+    <label for="color">Selected Color: {color.hexString}</label>
+    <div>
+      <button id="color" {style} on:click={copy}>
+        <h1>{color.hexString}</h1>
+        {#if copying}
+          <h3>Copied!</h3>
+        {:else}
+          <h3>Copy to Clipboard</h3>
+        {/if}
+      </button>
     </div>
   </div>
 </main>
 
 <style>
-  #select {
-    margin-top: 1em;
-  }
-
-  #color {
+  button {
     width: 400px;
     border: 1px;
     border: solid;
@@ -57,7 +66,10 @@
     border-width: 1px;
     text-align: center;
     color: white;
-    -webkit-text-stroke: 1px black;
-    font-family: "Roboto Mono", "Courier New", Courier, monospace;
+    cursor: pointer;
+  }
+
+  #select {
+    margin-top: 1em;
   }
 </style>
